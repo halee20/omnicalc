913e03ce9913b5eeedceec0cf57da7aaf88caccf
Ha Lee <halee20@users.noreply.github.com>
2016-11-05 20:52:37 -0500

    1
    1

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -4,26 +4,60 @@ class CalculationsController < ApplicationController
     @text = params[:user_text]
     @special_word = params[:user_word]
 
-    # ================================================================================
+    # ==========================================================================
     # Your code goes below.
     # The text the user input is in the string @text.
     # The special word the user input is in the string @special_word.
-    # ================================================================================
+    # ==========================================================================
+
+    # Character count with spaces
+    # ===========================
+    @character_count_with_spaces = @text.length
 
 
-    @character_count_with_spaces = "Replace this string with your answer."
+    # Character count without spaces
+    # ==============================
 
-    @character_count_without_spaces = "Replace this string with your answer."
+    # Removing whitespace from string:
+    text_without_spaces = @text.gsub(" ",  "")
+    text_without_newlines = text_without_spaces.gsub("\n", "")
+    text_without_carriage_returns = text_without_newlines.gsub("\r", "")
+    text_without_tabs = text_without_carriage_returns.gsub("\t", "")
 
-    @word_count = "Replace this string with your answer."
+    # Alternatively, @text.gsub(/\s+/, "") would remove all whitespace at once.
 
-    @occurrences = "Replace this string with your answer."
+    @character_count_without_spaces = text_without_tabs.length
 
-    # ================================================================================
-    # Your code goes above.
-    # ================================================================================
+    # Another alternative would be to count the occurrences of each whitespace
+    #   character, then subtract that from the total length:
+    # @text.length - @text.count(" ") - @text.count("\n") - etc
 
-    render("word_count.html.erb")
+    # Word Count
+    # ==========
+
+    array_of_words = @text.split
+    @word_count = array_of_words.count
+
+    # Occurrences
+    # ===========
+
+    text_without_periods            = @text.gsub(".", "")
+    text_without_commas             = text_without_periods.gsub(",", "")
+    text_without_exclamation_points = text_without_commas.gsub("!", "")
+    text_without_question_marks     = text_without_exclamation_points.gsub("?", "")
+    text_without_colons             = text_without_exclamation_points.gsub(":", "")
+    text_without_semicolons         = text_without_colons.gsub(";", "")
+    text_without_forward_slashes    = text_without_semicolons.gsub("/", "")
+    text_without_back_slashes       = text_without_forward_slashes.gsub("\\", "")
+
+    # Alternatively, @text.gsub(/[^a-z0-9\s]/i, '') would remove anything
+    #   except letters, digits, and whitespace all at once.
+
+    downcased_text = text_without_back_slashes.downcase
+
+    clean_word_array = downcased_text.split
+
+    @occurrences = clean_word_array.count(@special_word.downcase)
   end
 
   def loan_payment
@@ -31,83 +65,102 @@ class CalculationsController < ApplicationController
     @years = params[:number_of_years].to_i
     @principal = params[:principal_value].to_f
 
-    # ================================================================================
+    # ==========================================================================
     # Your code goes below.
     # The annual percentage rate the user input is in the decimal @apr.
     # The number of years the user input is in the integer @years.
     # The principal value the user input is in the decimal @principal.
-    # ================================================================================
+    # ==========================================================================
 
-    @monthly_payment = "Replace this string with your answer."
+    rate = @apr / 100 / 12
+    nper = @years * 12
 
-    # ================================================================================
-    # Your code goes above.
-    # ================================================================================
-
-    render("loan_payment.html.erb")
+    @monthly_payment = (rate * @principal)/(1 - (1 + rate)**-nper)
   end
 
   def time_between
     @starting = Chronic.parse(params[:starting_time])
     @ending = Chronic.parse(params[:ending_time])
 
-    # ================================================================================
+    # ==========================================================================
     # Your code goes below.
     # The start time is in the Time @starting.
     # The end time is in the Time @ending.
     # Note: Ruby stores Times in terms of seconds since Jan 1, 1970.
     #   So if you subtract one time from another, you will get an integer
     #   number of seconds as a result.
-    # ================================================================================
+    # ==========================================================================
 
-    @seconds = "Replace this string with your answer."
-    @minutes = "Replace this string with your answer."
-    @hours = "Replace this string with your answer."
-    @days = "Replace this string with your answer."
-    @weeks = "Replace this string with your answer."
-    @years = "Replace this string with your answer."
-
-    # ================================================================================
-    # Your code goes above.
-    # ================================================================================
-
-    render("time_between.html.erb")
+    @seconds = @ending - @starting
+    @minutes = @seconds / 1.minute # 1.minute is just shorthand for 1 * 60
+    @hours = @seconds / 1.hour # 1.hour is just shorthand for 1 * 3600
+    @days = @seconds / 1.day
+    @weeks = @seconds / 1.week
+    @years = @seconds / 1.year
   end
 
   def descriptive_statistics
     @numbers = params[:list_of_numbers].gsub(',', '').split.map(&:to_f)
 
-    # ================================================================================
+    # ==========================================================================
     # Your code goes below.
     # The numbers the user input are in the array @numbers.
-    # ================================================================================
+    # ==========================================================================
 
-    @sorted_numbers = "Replace this string with your answer."
+    @sorted_numbers = @numbers.sort
 
-    @count = "Replace this string with your answer."
+    @count = @numbers.count
 
-    @minimum = "Replace this string with your answer."
+    @minimum = @numbers.min
 
-    @maximum = "Replace this string with your answer."
+    @maximum = @numbers.max
 
-    @range = "Replace this string with your answer."
+    @range = @maximum - @minimum
 
-    @median = "Replace this string with your answer."
+    # Median
+    # ======
+    if @count.odd?
+      @median = @sorted_numbers[@count / 2]
+    else
+      left_of_middle = @sorted_numbers[(@count / 2) - 1]
+      right_of_middle = @sorted_numbers[(@count / 2)]
+      @median = (left_of_middle + right_of_middle) / 2
+    end
 
-    @sum = "Replace this string with your answer."
 
-    @mean = "Replace this string with your answer."
+    @sum = @numbers.sum
 
-    @variance = "Replace this string with your answer."
+    @mean = @sum / @count
 
-    @standard_deviation = "Replace this string with your answer."
+    # Variance
+    # ========
+    squared_differences = []
 
-    @mode = "Replace this string with your answer."
+    @numbers.each do |num|
+      difference = num - @mean
+      squared_difference = difference ** 2
+      squared_differences.push(squared_difference)
+    end
 
-    # ================================================================================
-    # Your code goes above.
-    # ================================================================================
+    @variance = squared_differences.sum / @count
 
-    render("descriptive_statistics.html.erb")
+
+    @standard_deviation = Math.sqrt(@variance)
+
+    # Mode
+    # ====
+
+    leader = nil
+    leader_count = 0
+
+    @numbers.each do |num|
+      occurrences = @numbers.count(num)
+      if occurrences > leader_count
+        leader = num
+        leader_count = occurrences
+      end
+    end
+
+    @mode = leader
   end
 end
